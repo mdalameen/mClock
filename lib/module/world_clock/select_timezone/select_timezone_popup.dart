8adaf5e55026a294755c5f48d7bd36f2f0d6b9a5
Mohamed Al Ameen <mdalameen.s@gmail.com>
2021-05-18 18:32:49 +0530

    Design update for timezone

--- lib/module/world_clock/select_timezone/select_timezone_popup.dart
+++ lib/module/world_clock/select_timezone/select_timezone_popup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mclock/common/app_colors.dart';
 import 'package:mclock/injector.dart';
 import 'package:mclock/module/world_clock/world_clock_viewmodel.dart';
 import 'package:timezone/timezone.dart';
@@ -24,15 +25,36 @@ class _SelectTimeZonePopupState extends State<SelectTimeZonePopup> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (_, i) {
-        String timeZone = timeZones[i];
-        return ListTile(
-          enabled: !wcVm.addedTimezones.contains(timeZone),
-          onTap: () => Navigator.pop(context, SelectTimezoneOut(timeZone, wcVm.locations[timeZone])),
-          title: Text(timeZone),
-        );
-      },
+    return Column(
+      children: [
+        Container(
+            alignment: Alignment.center,
+            width: double.infinity,
+            padding: EdgeInsets.all(15),
+            color: AppColors.clockOuter,
+            child: Text(
+              'Select Timezone',
+              style: TextStyle(color: Colors.black, fontSize: 16),
+            )),
+        SizedBox(height: 5),
+        Expanded(
+            child: ListView.separated(
+          padding: EdgeInsets.zero,
+          separatorBuilder: (_, i) => Divider(),
+          itemCount: timeZones.length,
+          itemBuilder: (_, i) {
+            String timeZone = timeZones[i];
+            return ListTile(
+              enabled: !wcVm.addedTimezones.contains(timeZone),
+              onTap: () => Navigator.pop(context, SelectTimezoneOut(timeZone, wcVm.locations[timeZone])),
+              trailing: Icon(Icons.chevron_right),
+              title: Text(
+                timeZone,
+              ),
+            );
+          },
+        ))
+      ],
     );
   }
 }
